925f738d4897306f2420c0bd1bd3c6203f02b510
ReenahMae <rcampilanan.reenahc@gmail.com>
2025-10-15 20:48:19 +0800

    Lab Exercise 10 Springboot project

---
+++ src/main/java/com/appdev/phishers/campilanang4/Campilanang4Application.java
@@ -0,0 +1,13 @@
+package com.appdev.phishers.campilanang4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Campilanang4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Campilanang4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/appdev/phishers/campilanang4/Campilanang4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.phishers.campilanang4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Campilanang4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
